8a17ce9d2bf55c8810f504a756a1a4c3e9a9ff24
anand13sethi <anandsethi4@gmail.com>
2019-02-01 13:02:15 +0530

    aggregation function completed.

--- 2018201100.py
+++ 2018201100.py
@@ -5,7 +5,7 @@ import sqlparse
 
 identifiers = []
 database_dict = {}
-
+is_distinct = False
 
 def read_metadata():
     if not os.path.exists('./files/metadata.txt'):
@@ -43,6 +43,115 @@ def validate_columns(column_list, table_list):
     else:
         return vaid_cols
 
+def check_valid_agg_syntax(attribute_list):
+    agg_funcs = ['sum', 'max', 'min', 'avg']
+    valid = True
+    agg_dict = {}
+    for item in attribute_list.split(", "):
+        if len(item) > 3 and item[0:3] in agg_funcs:
+            agg_dict[item[4:-1]] = item[0:3]
+        else:
+            valid = False
+            break
+    if not valid:
+        return valid
+    else:
+        return agg_dict
+
+
+def max_agg_func(col_name, table_name):
+    if not os.path.exists('./files/' + table_name + ".csv"):
+        print("[Table Error]: No Table Found.\n")
+        sys.exit()
+    col_index = database_dict[table_name].index(col_name)
+    with open('./files/' + table_name + ".csv") as file:
+        row = []
+        for line in file:
+            row.append(line.strip().split(",")[col_index])
+    big = float('-inf')
+    for item in row:
+        if int(item) > big:
+            big = int(item)
+    return big
+
+def min_agg_func(col_name, table_name):
+    if not os.path.exists('./files/' + table_name + ".csv"):
+        print("[Table Error]: No Table Found.\n")
+        sys.exit()
+    col_index = database_dict[table_name].index(col_name)
+    with open('./files/' + table_name + ".csv") as file:
+        row = []
+        for line in file:
+            row.append(line.strip().split(",")[col_index])
+    small = float('inf')
+    for item in row:
+        if int(item) < small:
+            small = int(item)
+    return small
+
+def sum_agg_func(col_name, table_name):
+    if not os.path.exists('./files/' + table_name + ".csv"):
+        print("[Table Error]: No Table Found.\n")
+        sys.exit()
+    col_index = database_dict[table_name].index(col_name)
+    with open('./files/' + table_name + ".csv") as file:
+        row = []
+        for line in file:
+            row.append(line.strip().split(",")[col_index])
+    summation = 0
+    for item in row:
+        summation += int(item)
+    return summation
+
+def avg_agg_func(col_name, table_name):
+    if not os.path.exists('./files/' + table_name + ".csv"):
+        print("[Table Error]: No Table Found.\n")
+        sys.exit()
+    col_index = database_dict[table_name].index(col_name)
+    with open('./files/' + table_name + ".csv") as file:
+        row = []
+        for line in file:
+            row.append(line.strip().split(",")[col_index])
+    avg = 0
+    for item in row:
+        avg += int(item)
+    return avg/len(row) 
+
+
+def project_aggregation(agg_dict, table_name):
+    valid = True
+    for key in list(agg_dict.keys()):
+        if key not in database_dict[table_name]:
+            valid = False
+            break
+    if valid:
+        final_table = []
+        header = []
+        for key in agg_dict:
+            if agg_dict[key].lower() == 'max':
+                temp_table = max_agg_func(key, table_name)
+                final_table.append(str(temp_table))
+                header.append(table_name + "." + key)
+            elif agg_dict[key].lower() == 'min':
+                temp_table = min_agg_func(key, table_name)
+                final_table.append(str(temp_table))
+                header.append(table_name + "." + key)
+            elif agg_dict[key].lower() == 'sum':
+                temp_table = sum_agg_func(key, table_name)
+                final_table.append(str(temp_table))
+                header.append(table_name + "." + key)
+            else:
+                temp_table = avg_agg_func(key, table_name)
+                final_table.append(str(temp_table))
+                header.append(table_name + "." + key)
+        print("\t".join(header))
+        print("\t\t".join(final_table))
+    else:
+        print("[Attribute Error]: Invalid Arguments.\n")
+        sys.exit(1)
+
+
+
 def project_some_cols(index_list, table, table_list):
     header = []
     for col in list(index_list.keys()):
@@ -53,10 +162,22 @@ def project_some_cols(index_list, table, table_list):
     head = "\t".join(header)
     print(head)
 
-    for row in table:
-        for index in index_list.values():
-            print(str(row[index]) + "\t\t", end = " ")
-        print()
+    if is_distinct:
+        x = str()
+        distinct_set = set()
+        for row in table:
+            for index in index_list.values():
+                x += (str(row[index]) + "\t\t")
+            distinct_set.add(x)
+            x = str()
+        for item in distinct_set:
+            print(item)
+
+    else:    
+        for row in table:
+            for index in index_list.values():
+                print(str(row[index]) + "\t\t", end = " ")
+            print()
 
 
 def project_table(table, header):
@@ -131,14 +252,18 @@ def process_query(query):
             else:
                 project_some_cols(index_list, table, table_list)
         else:
-            return -1
+            agg = check_valid_agg_syntax(identifiers[1])
+            if agg is not False:
+                project_aggregation(agg, table_list[0].strip())
+            else:
+                return -1
 
     else:
         return -1
 
 
-
 def parse_query(query):
+    global is_distinct
     if ';' in query:
         query = query.strip(';')
     else:
@@ -153,6 +278,9 @@ def parse_query(query):
         for item in id:
             if str(item) != ';':
                 identifiers.append(str(item))
+        if 'distinct' in identifiers:
+            is_distinct = True
+            identifiers.pop(1)
         if len(identifiers) < 4:
             print("[Query Error]: Invalid Query.\n")
             sys.exit(1)
